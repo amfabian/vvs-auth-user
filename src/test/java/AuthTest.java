23d8ae37a7fafe82e92e03cf69ab1ca08e133344
Alexandre Fabian <xandy.fabian@gmail.com>
2022-11-11 17:24:53 -0300

    add Jacoco no Sonar

---
+++ src/test/java/AuthTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+
+import groovyjarjarantlr4.v4.runtime.misc.NotNull;
+import io.quarkus.test.junit.QuarkusTest;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+import javax.ws.rs.FormParam;
+
+@QuarkusTest
+public class AuthTest {
+
+    @Test
+    public void test(){
+
+        given().formParam("name", "Alexandre")
+                .formParam("email", "alex@mail.com")
+            .when().post("/auth/getjwt")
+            .then()
+                .statusCode(200);
+        
+    }
+    
+}
